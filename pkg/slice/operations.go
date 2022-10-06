4ad80502c7976ccf8126fe2ad2beab72b5504297
Mahendra Bagul <bagulm123@gmail.com>
2022-10-06 20:25:39 +0530

    added contains common slice of ints operation

--- pkg/slice/operations.go
+++ pkg/slice/operations.go
@@ -1,6 +1,15 @@
 package slice
 
-func Contains(haystack []string, needle string) bool {
+func ContainsInt(haystack []int, needle int) bool {
+	for _, s := range haystack {
+		if s == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsString(haystack []string, needle string) bool {
 	for _, s := range haystack {
 		if s == "*" || s == needle {
 			return true
